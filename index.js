9525bff79558e92e06fc637c811ff9276fbde324
zakir matin <zakir.matin@gmail.com>
2020-01-28 03:05:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Hello, Neil deGrasse Tyson!"
+var specialGuest = "Neil deGrasse Tyson!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
